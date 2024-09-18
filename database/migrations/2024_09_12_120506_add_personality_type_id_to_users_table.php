2fc2eb6f44ce135b092d20fc736bc8deeb8c4337
Phurinon <110959150+Phurinon@users.noreply.github.com>
2024-09-18 23:15:12 +0700

    Add files via upload

---
+++ database/migrations/2024_09_12_120506_add_personality_type_id_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('personality_type_id')->nullable()->after('email');
+            $table->foreign('personality_type_id')->references('id')->on('personality_types')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['personality_type_id']);
+            $table->dropColumn('personality_type_id');
+        });
+    }
+};
